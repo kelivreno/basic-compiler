f81e57055224d6f5799039f89594f921c3e11665
Kelangit Hakim <90366335+kelivreno@users.noreply.github.com>
2026-05-20 15:42:31 -0700

    Initialize the constructor so that it keeps track of the current position

--- lexer.py
+++ lexer.py
@@ -1,7 +1,10 @@
 class Lexer:
     
-    def __init__(self):
-        pass
+    def __init__(self, source):
+        self.source = source + '\n' # Source code to lex as a string. Append a new line to simplify lexing/parsing the last token/statement.
+        self.curChar = '' # Current character in the string.
+        self.curPos = -1 # Current position in the string.
+        self.nextChar()
     # Process the next character
     def nextChar(self):
         pass
